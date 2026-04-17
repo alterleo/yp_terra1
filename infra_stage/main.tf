b51cb2c5030febbcfd69718d3e43b3005c386a86
AlexK <a-l-e-k-s@yandex.ru>
2026-04-17 11:42:40 +0300

    variables topic

--- infra_stage/main.tf
+++ infra_stage/main.tf
@@ -1,32 +1,22 @@
-# Создание VPC и подсети
-resource "yandex_vpc_network" "this" {
-  name = "private"
-}
-
-resource "yandex_vpc_subnet" "private" {
-  name           = "private"
-  zone           = "ru-central1-a"
-  v4_cidr_blocks = ["192.168.10.0/24"]
-  network_id     = yandex_vpc_network.this.id
-}
-
 # Создание диска и виртуальной машины
 resource "yandex_compute_disk" "boot_disk" {
-  name     = "boot-disk"
-  zone     = "ru-central1-a"
-  image_id = "fd8ba9d5mfvlncknt2kd" # Ubuntu 22.04 LTS
-  size     = 15
+  name     = "${var.name_prefix}-boot-disk"
+  zone     = var.zone
+  image_id = var.image_id
+  
+  type = var.instance_resources.disk.disk_type
+  size = var.instance_resources.disk.disk_size
 }
 
 resource "yandex_compute_instance" "this" {
-  name                      = "linux-vm"
+  name                      = "${var.name_prefix}-linux-vm"
   allow_stopping_for_update = true
-  platform_id               = "standard-v3"
-  zone                      = "ru-central1-a"
+  platform_id               = var.instance_resources.platform_id
+  zone                      = var.zone
 
   resources {
-    cores  = "2"
-    memory = "4"
+    cores  = var.instance_resources.cores
+    memory = var.instance_resources.memory
   }
 
   boot_disk {
@@ -38,20 +28,32 @@ resource "yandex_compute_instance" "this" {
   }
 }
 
+# Создание VPC и подсети
+resource "yandex_vpc_network" "this" {
+  name = "${var.name_prefix}-private"
+}
+
+resource "yandex_vpc_subnet" "private" {
+  name           = keys(var.subnets)[0]
+  zone           = var.zone
+  v4_cidr_blocks = var.subnets[keys(var.subnets)[0]]
+  network_id     = yandex_vpc_network.this.id
+}
+
 # Создание Yandex Managed Service for YDB
 resource "yandex_ydb_database_serverless" "this" {
-  name        = "test-ydb-serverless"
+  name        = "${var.name_prefix}-ydb-serverless"
   location_id = "ru-central1"
 }
 
 # Создание сервисного аккаунта 
 resource "yandex_iam_service_account" "bucket" {
-  name = "bucket-sa"
+  name = "${var.name_prefix}-bucket-sa"
 }
 
 # Назначение роли сервисному аккаунту
 resource "yandex_resourcemanager_folder_iam_member" "storage_editor" {
-  folder_id = "b1g8ivlb505qm8g88tvh"
+  folder_id = var.folder_id
   role      = "storage.editor"
   member    = "serviceAccount:${yandex_iam_service_account.bucket.id}"
 }
@@ -64,7 +66,7 @@ resource "yandex_iam_service_account_static_access_key" "this" {
 
 # Создание бакета 
 resource "yandex_storage_bucket" "this" {
-  bucket     = "terraform-bucket-${random_string.bucket_name.result}"
+  bucket     = "${var.name_prefix}-terraform-bucket-${random_string.bucket_name.result}"
   access_key = yandex_iam_service_account_static_access_key.this.access_key
   secret_key = yandex_iam_service_account_static_access_key.this.secret_key
   
---
+++ infra_stage/terraform.tfvars
@@ -0,0 +1,9 @@
+folder_id = "b1g8ivlb505qm8g88tvh"
+name_prefix = "project-dev"
+subnets = {
+  "project-dev-subnet" = ["192.168.11.0/24"]
+}
+instance_resources = {
+  cores  = 2
+  memory = 2
+}
